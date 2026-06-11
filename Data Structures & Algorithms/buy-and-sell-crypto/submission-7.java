557935ff89cb6a7ccfcbb226cb400c7503c3a2c6
Bhudev Sahu <bhudevsahu@yahoo.in>
2026-06-11 10:58:59 -0700

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int l =0;
+        int maxP = 0;
+
+        for (int r = 1; r < prices.length; r++) {
+            if (prices[r] > prices[l]) {
+                int profit = prices[r] - prices[l];
+                maxP = Math.max(profit, maxP);
+            } else {
+                l = r;
+            }
+        }
+        return maxP;
+    }
+}
